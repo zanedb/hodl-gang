ec58213555ab8621a1803661bb651de393495397
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2018-04-07 19:01:30 -0400

    😀

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import { Box, Link as A } from '@hackclub/design-system'
-import '../App.css';
+import '../App.css'
 
 const Footer = () => (
-  <Box p={3} align="center" fontSize={30} color="#FFFFFF" bg="#2a9134">
-    Created by {' '}
+  <Box p={3} align="center" fontSize={3} color="white" bg="#2a9134">
+    Created by{' '}
     <A href="https://keybase.io/zanedb" underline>
       @zanedb
     </A>
