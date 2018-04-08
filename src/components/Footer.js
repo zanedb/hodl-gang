a782fe827788b5d36e9af091518b0e3ea2354192
Zane <contact@zane.sh>
2018-04-08 02:01:01 -0700

    Increase Footer font size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Box, Link as A } from '@hackclub/design-system'
 import '../App.css'
 
 const Footer = () => (
-  <Box p={3} align="center" fontSize={3} color="white" bg="#dacc3e">
+  <Box p={3} align="center" fontSize={4} color="white" bg="#dacc3e">
     Created by{' '}
     <A href="https://keybase.io/zanedb" underline>
       @zanedb
