a8d56f1ad10a2a53455135b76fb8801a9db3d9e3
Zane <contact@zane.sh>
2018-04-07 23:39:59 -0700

    Add Title component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { ThemeProvider } from '@hackclub/design-system'
 import './App.css';
 
 import Footer from './components/Footer'
+import Title from './components/Title'
 
 export default () => (
   <ThemeProvider>
+    <Title />
     <Footer />
   </ThemeProvider>
 )
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Section, Heading } from '@hackclub/design-system'
+import '../App.css'
+
+const Title = () => (
+  <Section bg="accent">
+    <Heading f={7}>
+      #HODLGANG
+    </Heading>
+  </Section>
+)
+
+export default Title
