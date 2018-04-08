7327e4a6545ec4703f7c95dc2a76dfeced36953a
Zane <contact@zane.sh>
2018-04-08 02:25:50 -0700

    Add unique title to iframe (fixes jsx-a11y/iframe-has-title warning)

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -4,7 +4,7 @@ import '../App.css'
 
 const Embed = () => (
   <Section bg="#ff495c">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/JZYZoQQ6LJQ?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe title="#HODLGANG" width="560" height="315" src="https://www.youtube.com/embed/JZYZoQQ6LJQ?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </Section>
 )
 
