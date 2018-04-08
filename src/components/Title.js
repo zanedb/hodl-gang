376aab8c32d770ea1a37b93c1f5259cfda3b7259
Zane <contact@zane.sh>
2018-04-08 01:48:18 -0700

    Add more detail to Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Section, Heading } from '@hackclub/design-system'
+import { Section, Heading, Text } from '@hackclub/design-system'
 import '../App.css'
 import data from './../data.json'
 
 const Title = () => (
   <Section bg="accent">
     <Heading f={7}>
-      {data.name}
+      <Text fontSize={84}>
+        {data.name}
+      </Text>
+      <Text fontSize={4}>
+        {data.description}
+      </Text>
     </Heading>
   </Section>
 )
