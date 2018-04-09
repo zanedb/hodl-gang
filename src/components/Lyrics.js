518ca4dd2bbc25f8b3d4ee5d2b210f1cfaea6ac8
Zane <contact@zane.sh>
2018-04-08 17:50:06 -0700

    Update Lyrics spacing Why did I not do this at first?

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -8,171 +8,171 @@ const Lyrics = () => (
     </Text>
     {/* PREPARE FOR SERIOUS SPAGHETTI */}
     <Text ml={400} mr={400} mt={20} mb={20} fontSize={22}>
-      {'[Intro]'}
+      [Intro]
       <br/>
-      {'Bitcoin, and things like it, is the equivalent of the red pill'}
+      Bitcoin, and things like it, is the equivalent of the red pill
       <br/>
-      {'This is a technology, it\'s a computing language, and I can build an app on it'}
+      This is a technology, it's a computing language, and I can build an app on it
       <br/>
-      {'Is Bitcoin the currency of the future?'}
+      Is Bitcoin the currency of the future?
       <br/>
       <br/>
-      {'[Chorus x2]'}
+      [Chorus x2]
       <br/>
-      {'Crypto gang'}
+      Crypto gang
       <br/>
-      {'Ether gang'}
+      Ether gang
       <br/>
-      {'Bit gang'}
+      Bit gang
       <br/>
-      {'Alt gang'}
+      Alt gang
       <br/>
-      {'Lite gang'}
+      Lite gang
       <br/>
-      {'Cash game'}
+      Cash game
       <br/>
-      {'Blockchain (woah)'}
+      Blockchain (woah)
       <br/>
-      {'I don\'t really ever buy stocks man (nope)'}
+      I don't really ever buy stocks man (nope)
       <br/>
-      {'Haters wanna say I\'m in a bubble man! (blow)'}
+      Haters wanna say I'm in a bubble man! (blow)
       <br/>
-      {'Ch-chew em up like bubble gum (dope)'}
+      Ch-chew em up like bubble gum (dope)
       <br/>
-      {'Just made a mil with my pocket change (yo)'}
+      Just made a mil with my pocket change (yo)
       <br/>
-      {'Record bout to go and join the hodl gang (oh)'}
+      Record bout to go and join the hodl gang (oh)
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG'}
+      HODL GANG, HODL GANG, HODL GANG
       <br/>
       <br/>
-      {'[Verse 1: Chris Record]'}
+      [Verse 1: Chris Record]
       <br/>
-      {'Ethereum pays for my rent (boss)'}
+      Ethereum pays for my rent (boss)
       <br/>
-      {'Bitcoin pays for the Bent (skirt)'}
+      Bitcoin pays for the Bent (skirt)
       <br/>
-      {'Litecoin pays for the jet (fleet)'}
+      Litecoin pays for the jet (fleet)
       <br/>
-      {'Bitcoin Cash for the rest (blast)'}
+      Bitcoin Cash for the rest (blast)
       <br/>
-      {'Spend 30 racks on an ICO, what coin did you buy? I forgot yo!'}
+      Spend 30 racks on an ICO, what coin did you buy? I forgot yo!
       <br/>
-      {'Just got started last night bro, and I\'m already hooked like a pipe though'}
+      Just got started last night bro, and I'm already hooked like a pipe though
       <br/>
-      {'Flippin the coin, making it grow, taking it slow, letting it go, cashflow\'s growing it\'s incredible to watch it flow'}
+      Flippin the coin, making it grow, taking it slow, letting it go, cashflow's growing it's incredible to watch it flow
       <br/>
-      {'Stackin the cash, taking it home, making it fast, I\'m on a roll, a little bit mo\' is all I ask for the bubble though'}
+      Stackin the cash, taking it home, making it fast, I'm on a roll, a little bit mo' is all I ask for the bubble though
       <br/>
-      {'Currently, learning \'bout a new type of currency'}
+      Currently, learning 'bout a new type of currency
       <br/>
-      {'Burning the dollar down like an emergency, urgently listen my friends have been urging me, coins have been surging we earning like surgeons see'}
+      Burning the dollar down like an emergency, urgently listen my friends have been urging me, coins have been surging we earning like surgeons see
       <br/>
-      {'We on a mission man, cryptomaniacs, act like you don\'t really know about us? Stop that!'}
+      We on a mission man, cryptomaniacs, act like you don't really know about us? Stop that!
       <br/>
-      {'Bitcoin blowing up like a bomb, step back! Used to be a couple hundred dollars, should a copped that!'}
+      Bitcoin blowing up like a bomb, step back! Used to be a couple hundred dollars, should a copped that!
       <br/>
-      {'Blew up on a scene from the mind of a brainiac, back in 2009, where the miners at?'}
+      Blew up on a scene from the mind of a brainiac, back in 2009, where the miners at?
       <br/>
-      {'Silk road baby, it\'s gotta be anonymous (skirt)'}
+      Silk road baby, it's gotta be anonymous (skirt)
       <br/>
-      {'Satoshi blew up now we on the map! (yea)'}
+      Satoshi blew up now we on the map! (yea)
       <br/>
       <br/>
-      {'[Chorus]'}
+      [Chorus]
       <br/>
-      {'Crypto gang'}
+      Crypto gang
       <br/>
-      {'Ether gang'}
+      Ether gang
       <br/>
-      {'Bit gang'}
+      Bit gang
       <br/>
-      {'Alt gang'}
+      Alt gang
       <br/>
-      {'Lite gang'}
+      Lite gang
       <br/>
-      {'Cash game'}
+      Cash game
       <br/>
-      {'Blockchain (woah)'}
+      Blockchain (woah)
       <br/>
-      {'I don\'t really ever buy stocks man (nope)'}
+      I don't really ever buy stocks man (nope)
       <br/>
-      {'Haters wanna say I\'m in a bubble man! (blow)'}
+      Haters wanna say I'm in a bubble man! (blow)
       <br/>
-      {'Ch-chew em up like bubble gum (dope)'}
+      Ch-chew em up like bubble gum (dope)
       <br/>
-      {'Just made a mil with my pocket change (yo)'}
+      Just made a mil with my pocket change (yo)
       <br/>
-      {'Record bout to go and join the hodl gang (oh)'}
+      Record bout to go and join the hodl gang (oh)
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG'}
+      HODL GANG, HODL GANG, HODL GANG
       <br/>
       <br/>
-      {'[Verse 2: Chris Record]'}
+      [Verse 2: Chris Record]
       <br/>
-      {'Everybody looking at me now and they shocked now \'cuz the price going down, meanwhile...'}
+      Everybody looking at me now and they shocked now 'cuz the price going down, meanwhile...
       <br/>
-      {'I\'m a cop more at a discount, with a smile, cuz I\'m making money on the Alt coins right now!'}
+      I'm a cop more at a discount, with a smile, cuz I'm making money on the Alt coins right now!
       <br/>
-      {'I\'m a double down like animal style (sup now)'}
+      I'm a double down like animal style (sup now)
       <br/>
-      {'People in the crowd they watching me bubble up now'}
+      People in the crowd they watching me bubble up now
       <br/>
-      {'Open up a brand new Coinbase account and I\'m buying everything from Ripple to Litecoin now!'}
+      Open up a brand new Coinbase account and I'm buying everything from Ripple to Litecoin now!
       <br/>
-      {'Pull out my principal keep it real simple then play with the house (facts)'}
+      Pull out my principal keep it real simple then play with the house (facts)
       <br/>
-      {'Investing in crypto, I\'m chasing these coins like they cheese to a mouse (mouse)'}
+      Investing in crypto, I'm chasing these coins like they cheese to a mouse (mouse)
       <br/>
-      {'I\'m on the verge of a digital currency merging technologies this is the future so obviously I\'m all in, there\'s no bottom we #hodling now!'}
+      I'm on the verge of a digital currency merging technologies this is the future so obviously I'm all in, there's no bottom we #hodling now!
       <br/>
       <br/>
-      {'[Chorus]'}
+      [Chorus]
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG'}
+      HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG
       <br/>
-      {'Throw your hands in the air man (fly)'}
+      Throw your hands in the air man (fly)
       <br/>
-      {'If you’re d-down with the blockchain (bbbrrrrr)'}
+      If you’re d-down with the blockchain (bbbrrrrr)
       <br/>
-      {'I ain’t got time for the haters man (no)'}
+      I ain’t got time for the haters man (no)
       <br/>
-      {'We about to grow to a million (grow)'}
+      We about to grow to a million (grow)
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG (ohhhh)'}
+      HODL GANG, HODL GANG, HODL GANG (ohhhh)
       <br/>
-      {'Tell me, are you down with the crypto?'}
+      Tell me, are you down with the crypto?
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG'}
+      HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG, HODL GANG
       <br/>
-      {'Throw your hands in the air man (fly)'}
+      Throw your hands in the air man (fly)
       <br/>
-      {'If you’re d-down with the blockchain (bbbrrrrr)'}
+      If you’re d-down with the blockchain (bbbrrrrr)
       <br/>
-      {'I ain’t got time for the haters man (no)'}
+      I ain’t got time for the haters man (no)
       <br/>
-      {'We about to grow to a million (grow)'}
+      We about to grow to a million (grow)
       <br/>
-      {'HODL GANG, HODL GANG, HODL GANG (ohhhh)'}
+      HODL GANG, HODL GANG, HODL GANG (ohhhh)
       <br/>
-      {'Tell me, are you down with the crypto?'}
+      Tell me, are you down with the crypto?
       <br/>
       <br/>
-      {'[Outro]'}
+      [Outro]
       <br/>
-      {'It is the most powerful technology that the world has seen, I believe, since the invention of agriculture'}
+      It is the most powerful technology that the world has seen, I believe, since the invention of agriculture
       <br/>
-      {'Bitcoin is exciting because it shows how cheap it can be'}
+      Bitcoin is exciting because it shows how cheap it can be
       <br/>
-      {'Bitcoin is, better than currency'}
+      Bitcoin is, better than currency
       <br/>
-      {'As with most major technology shifts, let\'s think about what young people are doing'}
+      As with most major technology shifts, let's think about what young people are doing
       <br/>
-      {'32% of young people say they prefer Bitcoin to stocks'}
+      32% of young people say they prefer Bitcoin to stocks
       <br/>
-      {'42% of millennial males say they plan to purchase Bitcoin in the next 5 years'}
+      42% of millennial males say they plan to purchase Bitcoin in the next 5 years
       <br/>
-      {'We\'re barely in the first inning right now'}
+      We're barely in the first inning right now
       <br/>
     </Text>
   </Box>
