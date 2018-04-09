08d8c2fba594e6aa35c70470469a44dd9cb928dc
Zane <contact@zane.sh>
2018-04-08 17:53:49 -0700

    Warn the people Look, it's not _seriously_ the best song.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Link as A } from '@hackclub/design-system'
+import { Box, Link as A, Text } from '@hackclub/design-system'
 
 const Footer = () => (
   <Box p={3} align="center" fontSize={4} color="white" bg="#dacc3e">
@@ -7,6 +7,9 @@ const Footer = () => (
     <A href="https://keybase.io/zanedb" underline>
       @zanedb
     </A>
+    <Text fontSize={2}>
+      (don't worry, it's ironic)
+    </Text>
   </Box>
 )
 
